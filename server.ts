bd98101f2bc6c789b6bff09cdc45b3435da55986
samarthgad68 <samarth.gad68@gmail.com>
2026-08-26 11:35:21 +0530

    Update

--- server.ts
+++ server.ts
@@ -985,7 +985,7 @@ Dialogue: 0,0:00:00.00,0:01:00.00,Default,,0,0,400,,{\\b1\\pos(540,1555)}${assSa
 // Ensure paths are safely escaped for FFmpeg filtergraph syntax
     const safeAssPath = assFilePath.replace(/\\/g, '/').replace(/:/g, '\\:').replace(/'/g, "\\'");
     const safeFontDir = fontDir.replace(/\\/g, '/').replace(/:/g, '\\:').replace(/'/g, "\\'");
-    const ffmpegCmdPrimary = `"${ffmpegBin}" -nostdin -threads 1 -y -i "${templateFilePath}" -i "${photoCirclePath}" -filter_complex "[0:v]scale=320:568[v0];[v0][1:v]overlay=43:183:eval=init[v1];[v1]ass='${safeAssPath}':fontsdir='${safeFontDir}'[vout]" -map "[vout]" -map 0:a? -c:v libx264 -preset ultrafast -crf 85 -pix_fmt yuv420p -c:a aac -b:a 32k -ar 11025 -ac 1 -max_muxing_queue_size 256 -movflags +faststart "${outputMp4Path}"`;
+    const ffmpegCmdPrimary = `"${ffmpegBin}" -nostdin -threads 1 -y -i "${templateFilePath}" -i "${photoCirclePath}" -filter_complex "[0:v]scale=1080:1920[v0];[v0][1:v]overlay=145:610:eval=init[v1];[v1]ass='${safeAssPath}':fontsdir='${safeFontDir}'[vout]" -map "[vout]" -map 0:a? -c:v libx264 -preset ultrafast -crf 23 -pix_fmt yuv420p -c:a aac -b:a 128k -ar 44100 -ac 2 -max_muxing_queue_size 1024 -movflags +faststart "${outputMp4Path}"`;
     const ffmpegEnv = {
         ...process.env,
         FONTCONFIG_FILE: process.env.FONTCONFIG_FILE || '/tmp/fonts/fonts.conf',
